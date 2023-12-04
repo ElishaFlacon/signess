fead30fa55b792bcdbfc47882448e74dc406c6b0
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-12-04 19:12:49 +0500

    update example

--- example/example.py
+++ example/example.py
@@ -8,7 +8,7 @@ from fedot.core.utils import set_random_seed
 
 
 def create_dataset(path_to_data):
-    # create Dataet class
+    # create Dataset class
     ds = Dataset(
         autograph=Autograph(size=(380, 380)),
         document=Document()
@@ -27,46 +27,45 @@ def create_network(path_to_dataset, path_to_picture, path_to_save):
     # train model with loaded dataset by 3 epochs
     network.train(dataset=dataset, num_epochs=3)
 
-    # check accuracy
-    accuracy = network.accuracy(dataset)
-    print(f'Accuracy: {accuracy}')
+    # predicts dataset
+    predicts = network.predict(dataset)
+    print(f'Predicts: {predicts}')
 
     # classify picture
     predict = network.classify(path_to_picture, path_to_dataset)
-    print(f'Classify: {predict}')
 
     # save model
     network.save(path_to_save)
     print('Model Save!')
 
-    return (accuracy, predict)
+    return (predicts, predict)
 
 
 def load_network(path_to_dataset, path_to_picture, path_to_load):
-    # create new Networ
+    # create new Network
     new_network = FedotCNN()
     # load model
     new_network.load(path_to_load)
     # load dataset where path - path to .npz file
     dataset = new_network.load_dataset(path=path_to_dataset)
 
-    # check accuracy on new model
-    accuracy = new_network.accuracy(dataset)
-    print(f'Accuracy: {accuracy}')
+    # predicts dataset on new model
+    predicts = new_network.predict(dataset)
+    print(f'Predicts: {predicts}')
 
     # classify picture on new model
     predict = new_network.classify(path_to_picture, path_to_dataset)
     print(f'Classify: {predict}')
 
-    return (accuracy, predict)
+    return (predicts, predict)
 
 
 if __name__ == '__main__':
     set_random_seed(1)
 
     # config
-    path_to_data = './docs'
-    path_to_picture = './result/autographs/0-first_person-0.png'
+    path_to_data = './data.dev'
+    path_to_picture = './result/autographs/11-Khalin-8.png'
     path_to_save_and_load = './model'
 
     path_to_dataset = create_dataset(path_to_data)
